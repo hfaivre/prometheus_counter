ae0f9e1545b78906947137734e6517305c4aaf5c
Simon Guerrier <simon.guerrier@datadoghq.com>
2019-06-27 17:45:17 +0200

    added rand counter and commented linear

--- prom_counter.py
+++ prom_counter.py
@@ -1,4 +1,4 @@
-from prometheus_client import start_http_server, Counter
+from prometheus_client import start_http_server, Counter, Gauge
 from prometheus_client.parser import text_string_to_metric_families
 import random
 import time
@@ -20,36 +20,47 @@ log.setLevel(logging.INFO)
 if __name__ == '__main__':
     start_http_server(8000)
 
-    c = Counter('static_increment_counter', 'Counter that increments by 2 every 2 seconds')
-    g = Counter('static_increment_gauge', 'Gauge that increments by 2 every 2 seconds')
+    c = Counter('static_increment_counter', 'Counter that increments by 50 every 2 seconds')
+    g = Gauge('static_increment_gauge', 'Gauge that increments by 50 every 2 seconds')
 
 
-    c_lin = Counter('linear_increment_counter', 'Counter that increments in a linear fashion 2 every 2 seconds')
-    g_lin = Counter('linear_increment_gauge', 'Gauge that increments in a linear fashion every 2 seconds')
-    i  = 100
+    #c_lin = Counter('linear_increment_counter', 'Counter that increments in a linear fashion every 15 seconds')
+    #g_lin = Gauge('linear_increment_gauge', 'Gauge that increments in a linear fashion every 15 seconds')
+
+    c_rand = Counter('random_increment_counter', 'Counter that increments in a random fashion every 15 seconds')
+    g_rand = Gauge('random_increment_gauge', 'Gauge that increments in a random fashion every 15 seconds')
+
+    #i  = 1
     run = 0
+
+    random.seed()
+
     while True:
     	
     	log.info("*******RUN %i **********",run)
 
     	# Increment the counter and gauge by the same value at each run
-    	c.inc(2)
-    	g.inc(2)
+    	c.inc(50)
+    	g.inc(50)
 
     	# Increment the counter and gauge by an increasing increment
-    	c_lin.inc(i)
-    	g_lin.inc(i)
+    	#c_lin.inc(i)
+    	#g_lin.inc(i)
 
+        rand = random.randint(1,1000)
+        c_rand.inc(rand)
+        g_rand.inc(rand)
+        log.info("Incremented Rand by {0}".format(rand))
     	 # Scrape endpoint to get metric values logged to stdout
     	metrics = requests.get("http://localhost:8000/").content
     	for family in text_string_to_metric_families(metrics.decode()):
     		for sample in family.samples:
     			log.info("Name: {0} Labels: {1}  Value: {2}".format(*sample))
 
-    	time.sleep(2)
+    	time.sleep(15)
 
 
-    	i+=2
+    	#i+=2
     	run+=1
 
 
